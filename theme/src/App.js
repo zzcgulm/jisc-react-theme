cd70f95eb095f87089ae176b51f7e131d8a08e2d
zzcgulm <leigh.morris@jisc.ac.uk>
2020-03-20 10:44:27 +0000

    Add minimal Jisc banner, using plain CSS.

--- theme/src/App.js
+++ theme/src/App.js
@@ -1,23 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+
+import logo from './static/images/jisc-logo.svg';
+
+
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+
+
+        <div className="jiscBanner">
+
+          <div className="sitewideContainer">
+
+            <a
+              className="jiscLink"
+              href="https://www.jisc.ac.uk/"
+              title="Go to the Jisc homepage"
+            >
+              <img
+                src={logo}
+                className="jiscLogo"
+                alt="Jisc" 
+              />
+            </a>
+
+          </div>
+
+        </div>
+
+
+
       </header>
     </div>
   );
