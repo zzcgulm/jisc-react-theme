9e2e2c00ececd6599809d74226c7df0848ccddba
zzcgulm <leigh.morris@jisc.ac.uk>
2020-03-20 15:25:51 +0000

    Add empty theme.

--- theme/src/App.js
+++ theme/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import { MuiThemeProvider, withStyles } from "@material-ui/core";
+import theme from './config/theme.styles';
+
 import { Container } from '@material-ui/core';
 import { Grid } from '@material-ui/core';
 import { Link } from '@material-ui/core';
@@ -16,196 +19,197 @@ import logo from './static/images/jisc-logo.svg';
 function App() {
   return (
     <div className="App bg2">
+      <MuiThemeProvider theme={theme}>
 
 
 
 
-
-      <header className="App-header">
-
-
-
-        <div className="jiscBanner">
-
-          <Container maxWidth="md">
-
-            <a
-              className="jiscLink"
-              href="https://www.jisc.ac.uk/"
-              title="Go to the Jisc homepage"
-            >
-              <img
-                src={logo}
-                className="jiscLogo"
-                alt="Jisc" 
-              />
-            </a>
-
-          </Container>
-
-        </div>
+        <header className="App-header">
 
 
 
-      </header>
+          <div className="jiscBanner">
 
+            <Container maxWidth="md">
 
-
-
-
-      <footer className="jiscFooter">
-        <h2 className="visuallyHidden">Site footer</h2>
-
-
-
-        <section className="bg4">
-
-          <Container maxWidth="md">
-            <Grid container spacing={2}>
-              <Grid item sm={6}>
-
-                <Typography variant="h3" component="h3" gutterBottom="true">
-                  [Service name]
-                </Typography>
-                <Typography variant="body2" gutterBottom="true">
-                  Short service description. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </Typography>
-                <Typography variant="body2" gutterBottom="true">
-                  <Link href="TODO" underline="always" variant="body2">Give feedback</Link>
-                </Typography>
-
-              </Grid>
-              <Grid item sm={3}>
-
-                <Typography variant="h3" component="h4" gutterBottom="true">
-                  [Primary navigation]
-                </Typography>
-                <ul className="linkList">
-                  <li className="linkListItem">
-                    <Link href="TODO" underline="always" variant="body2">Link</Link>
-                  </li>
-                  <li className="linkListItem">
-                    <Link href="TODO" underline="always" variant="body2">Link</Link>
-                  </li>
-                  <li className="linkListItem">
-                    <Link href="TODO" underline="always" variant="body2">Link</Link>
-                  </li>
-                  <li className="linkListItem">
-                    <Link href="TODO" underline="always" variant="body2">Link</Link>
-                  </li>
-                  <li className="linkListItem">
-                    <Link href="TODO" underline="always" variant="body2">Link</Link>
-                  </li>
-                </ul>
-
-              </Grid>
-              <Grid item sm={3}>
-
-                <Typography variant="h3" component="h4" gutterBottom="true">
-                  [Service utilities]
-                </Typography>
-                <ul className="linkList">
-                  <li className="linkListItem">
-                    <Link href="TODO" underline="always" variant="body2">Link</Link>
-                  </li>
-                  <li className="linkListItem">
-                    <Link href="TODO" underline="always" variant="body2">Link</Link>
-                  </li>
-                  <li className="linkListItem">
-                    <Link href="TODO" underline="always" variant="body2">Link</Link>
-                  </li>
-                </ul>
-
-              </Grid>
-            </Grid>
-          </Container>
-
-        </section>
-
-
-
-        <section className="bg5">
-
-          <Container maxWidth="md">
-            <Grid container spacing={2}>
-              <Grid item sm={6}>
-
-                <h3 className="visuallyHidden">About Jisc</h3>
+              <a
+                className="jiscLink"
+                href="https://www.jisc.ac.uk/"
+                title="Go to the Jisc homepage"
+              >
                 <img
                   src={logo}
                   className="jiscLogo"
                   alt="Jisc" 
                 />
-                <Typography variant="body2" gutterBottom="true">
-                  Vision statement. We’re a membership organisation, providing digital solutions for UK education and research.
-                </Typography>
-                <Typography variant="body2" gutterBottom="true">
-                  <Link href="https://www.jisc.ac.uk" underline="always">jisc.ac.uk</Link>
-                </Typography>
+              </a>
 
+            </Container>
+
+          </div>
+
+
+
+        </header>
+
+
+
+
+
+        <footer className="jiscFooter">
+          <h2 className="visuallyHidden">Site footer</h2>
+
+
+
+          <section className="bg4">
+
+            <Container maxWidth="md">
+              <Grid container spacing={2}>
+                <Grid item sm={6}>
+
+                  <Typography variant="h3" component="h3" gutterBottom="true">
+                    [Service name]
+                  </Typography>
+                  <Typography variant="body2" gutterBottom="true">
+                    Short service description. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  </Typography>
+                  <Typography variant="body2" gutterBottom="true">
+                    <Link href="TODO" underline="always" variant="body2">Give feedback</Link>
+                  </Typography>
+
+                </Grid>
+                <Grid item sm={3}>
+
+                  <Typography variant="h3" component="h4" gutterBottom="true">
+                    [Primary navigation]
+                  </Typography>
+                  <ul className="linkList">
+                    <li className="linkListItem">
+                      <Link href="TODO" underline="always" variant="body2">Link</Link>
+                    </li>
+                    <li className="linkListItem">
+                      <Link href="TODO" underline="always" variant="body2">Link</Link>
+                    </li>
+                    <li className="linkListItem">
+                      <Link href="TODO" underline="always" variant="body2">Link</Link>
+                    </li>
+                    <li className="linkListItem">
+                      <Link href="TODO" underline="always" variant="body2">Link</Link>
+                    </li>
+                    <li className="linkListItem">
+                      <Link href="TODO" underline="always" variant="body2">Link</Link>
+                    </li>
+                  </ul>
+
+                </Grid>
+                <Grid item sm={3}>
+
+                  <Typography variant="h3" component="h4" gutterBottom="true">
+                    [Service utilities]
+                  </Typography>
+                  <ul className="linkList">
+                    <li className="linkListItem">
+                      <Link href="TODO" underline="always" variant="body2">Link</Link>
+                    </li>
+                    <li className="linkListItem">
+                      <Link href="TODO" underline="always" variant="body2">Link</Link>
+                    </li>
+                    <li className="linkListItem">
+                      <Link href="TODO" underline="always" variant="body2">Link</Link>
+                    </li>
+                  </ul>
+
+                </Grid>
               </Grid>
-              <Grid item sm={3}>
+            </Container>
 
-                <Typography variant="h3" component="h4" gutterBottom="true">
-                  Jisc product areas
-                </Typography>
-                <ul className="linkList">
-                  <li className="linkListItem">
-                    <Link href="TODO" underline="always" variant="body2">Link</Link>
-                  </li>
-                  <li className="linkListItem">
-                    <Link href="TODO" underline="always" variant="body2">Link</Link>
-                  </li>
-                  <li className="linkListItem">
-                    <Link href="TODO" underline="always" variant="body2">Link</Link>
-                  </li>
-                  <li className="linkListItem">
-                    <Link href="TODO" underline="always" variant="body2">Link</Link>
-                  </li>
-                  <li className="linkListItem">
-                    <Link href="TODO" underline="always" variant="body2">Link</Link>
-                  </li>
-                  <li className="linkListItem">
-                    <Link href="TODO" underline="always" variant="body2">Link</Link>
-                  </li>
-                  <li className="linkListItem">
-                    <Link href="TODO" underline="always" variant="body2">Link</Link>
-                  </li>
-                </ul>
+          </section>
 
+
+
+          <section className="bg5">
+
+            <Container maxWidth="md">
+              <Grid container spacing={2}>
+                <Grid item sm={6}>
+
+                  <h3 className="visuallyHidden">About Jisc</h3>
+                  <img
+                    src={logo}
+                    className="jiscLogo"
+                    alt="Jisc" 
+                  />
+                  <Typography variant="body2" gutterBottom="true">
+                    Vision statement. We’re a membership organisation, providing digital solutions for UK education and research.
+                  </Typography>
+                  <Typography variant="body2" gutterBottom="true">
+                    <Link href="https://www.jisc.ac.uk" underline="always">jisc.ac.uk</Link>
+                  </Typography>
+
+                </Grid>
+                <Grid item sm={3}>
+
+                  <Typography variant="h3" component="h4" gutterBottom="true">
+                    Jisc product areas
+                  </Typography>
+                  <ul className="linkList">
+                    <li className="linkListItem">
+                      <Link href="TODO" underline="always" variant="body2">Link</Link>
+                    </li>
+                    <li className="linkListItem">
+                      <Link href="TODO" underline="always" variant="body2">Link</Link>
+                    </li>
+                    <li className="linkListItem">
+                      <Link href="TODO" underline="always" variant="body2">Link</Link>
+                    </li>
+                    <li className="linkListItem">
+                      <Link href="TODO" underline="always" variant="body2">Link</Link>
+                    </li>
+                    <li className="linkListItem">
+                      <Link href="TODO" underline="always" variant="body2">Link</Link>
+                    </li>
+                    <li className="linkListItem">
+                      <Link href="TODO" underline="always" variant="body2">Link</Link>
+                    </li>
+                    <li className="linkListItem">
+                      <Link href="TODO" underline="always" variant="body2">Link</Link>
+                    </li>
+                  </ul>
+
+                </Grid>
               </Grid>
-            </Grid>
-          </Container>
+            </Container>
 
-        </section>
-
-
-
-        <section className="bg18">
-
-          <Container maxWidth="md">
-
-            <ul className="linkListInline">
-              <li className="linkListInlineItem">
-                <Link href="TODO" underline="always" variant="body2">Link</Link>
-              </li>
-              <li className="linkListInlineItem">
-                <Link href="TODO" underline="always" variant="body2">Link</Link>
-              </li>
-              <li className="linkListInlineItem">
-                <Link href="TODO" underline="always" variant="body2">Link</Link>
-              </li>
-            </ul>
-
-          </Container>
-
-        </section>
-
-      </footer>
+          </section>
 
 
 
+          <section className="bg18">
 
+            <Container maxWidth="md">
+
+              <ul className="linkListInline">
+                <li className="linkListInlineItem">
+                  <Link href="TODO" underline="always" variant="body2">Link</Link>
+                </li>
+                <li className="linkListInlineItem">
+                  <Link href="TODO" underline="always" variant="body2">Link</Link>
+                </li>
+                <li className="linkListInlineItem">
+                  <Link href="TODO" underline="always" variant="body2">Link</Link>
+                </li>
+              </ul>
+
+            </Container>
+
+          </section>
+
+        </footer>
+
+
+
+
+      </MuiThemeProvider>
     </div>
   );
 }
